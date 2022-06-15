6d67ac4d68aa2c1882fe56342ba71915bf1236d0
stbs22 <steveehs2000@gmail.com>
2022-06-15 09:10:08 -0400

    2022-15-06

--- Aplications/rewifi.sh
+++ Aplications/rewifi.sh
@@ -4,10 +4,15 @@
 notify-send -t 1000 -u low "Revisando crack"
 
 ping 8.8.8.8 -c 3 -W 3
+
 if [[ $? -eq 0 ]]; then
-       	notify-send -t 4000 -u normal "Conectado en:" "$(nmcli -t -f TYPE c show --active)"
+	notify-send -t 1000 -u normal "Conectado a $(printf "$(iw dev wlan0 link | grep SSID | awk -F: '{ print $2 }' | sed -e 's/^[ \t]*//')\n$(nmcli -t -f TYPE c show --active)")"
 else
-        notify-send -t 1000 -u critical "Fuera de Linea" "Intentando Reconectar"
+    notify-send -t 1000 -u critical "Fuera de Linea"
+fi
+
+'''
+		"Intentando Reconectar"
 			
 		for SSID in "Vet I" "UAI Alumnos" "iPhone de Esteban";
 		do     
@@ -24,4 +29,5 @@ else
 		done
 
 		notify-send -t 1500 -u critical "Fallo en Conexión"
-fi
+'''
+
